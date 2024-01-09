12f7a90ea63636e7b2906b7abd9df57f75d8dcb5
hemant0933 <116268777+hemant0933@users.noreply.github.com>
2024-01-09 10:04:04 +0530

    first commit

---
+++ src/services/store.js
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+// import { buildGetDefaultMiddleware } from "@reduxjs/toolkit/dist/getDefaultMiddleware";
+
+import { articleApi } from "./article";
+
+export const store = configureStore({
+  reducer: {
+    [articleApi.reducerPath]:articleApi.reducer
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(articleApi.middleware),
+});
